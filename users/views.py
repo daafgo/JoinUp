c96f0262577e521578dd982d39ec623d450aed58
dani <daafgo@gmail.com>
2024-03-22 22:34:15 +0000

    added control to skip send mail and sms on dev/test

--- users/views.py
+++ users/views.py
@@ -23,22 +23,23 @@ class SignupAPIView(APIView):
             if CustomUser.objects.filter(username=request.data['username']).exists():
                 return Response({'error': "This username it's in use"}, status=status.HTTP_400_BAD_REQUEST)
             user = serializer.save()
-            try:
-                subject = 'Register confirmation'
-                message = 'Please, confirm your email.'
-                email_from = settings.EMAIL_HOST_USER
-                recipient_list = [user.email]
-                send_mail(subject, message, email_from, recipient_list)
-            except Exception as e:
-                logger.error('error on mail send: ' + str(e))
-                return Response({'error sending email, please try again'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            try:
-                client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-                verify = client.verify.v2.services(settings.TWILIO_VERIFY_SERVICE_SID)
-                verify.verifications.create(to='+(34)' + user.phone, channel='sms')
-            except Exception as e:
-                logger.error('error on mail send: ' + str(e))
-                return Response({'error sending SMS, please try again'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            if settings.ENVIRONMENT == 'prod':
+                try:
+                    subject = 'Register confirmation'
+                    message = 'Please, confirm your email.'
+                    email_from = settings.EMAIL_HOST_USER
+                    recipient_list = [user.email]
+                    send_mail(subject, message, email_from, recipient_list)
+                except Exception as e:
+                    logger.error('error on mail send: ' + str(e))
+                    return Response({'error sending email, please try again'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                try:
+                    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+                    verify = client.verify.v2.services(settings.TWILIO_VERIFY_SERVICE_SID)
+                    verify.verifications.create(to='+(34)' + user.phone, channel='sms')
+                except Exception as e:
+                    logger.error('error on mail send: ' + str(e))
+                    return Response({'error sending SMS, please try again'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             return Response(
                 {"message": "Successful register, an email and a sms are sending to you to confirm your register."},
                 status=status.HTTP_201_CREATED)
